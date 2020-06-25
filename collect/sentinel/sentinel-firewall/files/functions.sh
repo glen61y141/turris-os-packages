481cf74d47e43cb5bd26d04f29886554624f1266
Karel Kočí <cynerd@email.cz>
2020-06-25 22:49:12 +0200

    collect: join common code from uci-defaults
    This was pretty mich the same code with exception of field to be set.
    All of those packages depend on sentinel-firewall and it is tied to
    actiovation of it. This makes sentinel-firewall an ideal package to
    place common code to.

---
+++ collect/sentinel/sentinel-firewall/files/functions.sh
@@ -0,0 +1,24 @@
+. /lib/functions.sh
+
+__enable_on_wan_zone() {
+	local section="$1"
+	local zone_name
+	config_get zone_name "$section" "name"
+	[ "$zone_name" != "wan" ] && return 0
+
+	local enabled
+	config_get_bool enabled "$section" "$option" ""
+	[ -n "$enabled" ] || \
+		uci -q set "firewall.$section.$option=1"
+}
+
+# This function enables given option on firewall zone wan unless it is already set
+# to some value.
+# option: option name to enable
+config_firewall_default_enable() (
+	local option="$1"
+	config_load "firewall"
+	config_foreach __enable_on_wan_zone "zone"
+	[ -z "$(uci changes firewall)" ] || \
+		uci commit firewall
+)
